3d1847401bc8c4ceb05f513716ca08b8e8192635
Kamal Singh <8919866+kamalpy@users.noreply.github.com>
2021-03-28 11:32:08 +0200

    feat: show quote of the day in homepage

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    qod_resp = requests.get("https://quotes.rest/qod?language=en")
+    if qod_resp.status_code == 200:
+        qod = qod_resp.json()
+        return qod["contents"]["quotes"][0]["quote"]
     return "Hello world from Azure"
